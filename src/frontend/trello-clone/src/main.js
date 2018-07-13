14f113eea9f0289efbc07911f31a5ff161a8c21d
Rajat Sethi <rajat22sethi@gmail.com>
2018-07-13 15:42:11 -0400

    Vuex scaffolding.

--- src/frontend/trello-clone/src/main.js
+++ src/frontend/trello-clone/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {store} from './store/store.js'
 
 Vue.config.productionTip = false
 
 Vue.use(Buefy.default) // Using Buefy CDN.
 
 new Vue({
+  store : store,
   render: h => h(App)
 }).$mount('#app')
